7f76c0f3cbd10e1330e3fc67f5103215c91d4e85
zriyaz <zakriariyaz@gmail.com>
2022-04-22 17:51:44 -0400

    Zakria - All buttons are linked

--- app/src/main/java/com/example/math_kidz/SubjectDifficultyAddition.java
+++ app/src/main/java/com/example/math_kidz/SubjectDifficultyAddition.java
@@ -3,6 +3,10 @@ package com.example.math_kidz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
+
 
 public class SubjectDifficultyAddition extends AppCompatActivity {
 
@@ -10,5 +14,39 @@ public class SubjectDifficultyAddition extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subject_difficulty_addition);
+
+        Button AddEasy = (Button) findViewById(R.id.AddEasy);
+        AddEasy.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),AdditionEasy.class);
+                startActivity(i);
+
+
+            }
+        });
+
+        Button AddMedium = (Button) findViewById(R.id.AddMedium);
+        AddMedium.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent ii = new Intent(getApplicationContext(),AdditionMedium.class);
+                startActivity(ii);
+
+
+            }
+        });
+
+        Button AddHard = (Button) findViewById(R.id.AddHard);
+        AddHard.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent iii = new Intent(getApplicationContext(),AdditionHard.class);
+                startActivity(iii);
+
+
+            }
+        });
+
+
     }
+
+
 }
--- app/src/main/java/com/example/math_kidz/SubjectDifficultyMultiplication.java
+++ app/src/main/java/com/example/math_kidz/SubjectDifficultyMultiplication.java
@@ -2,7 +2,10 @@ package com.example.math_kidz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SubjectDifficultyMultiplication extends AppCompatActivity {
 
@@ -10,5 +13,39 @@ public class SubjectDifficultyMultiplication extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subject_difficulty_multiplication);
+
+        Button MultEasy = (Button) findViewById(R.id.MultEasy);
+        MultEasy.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),MultiplicationEasy.class);
+                startActivity(i);
+
+
+            }
+        });
+
+        Button MultMedium = (Button) findViewById(R.id.MultMedium);
+        MultMedium.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent ii = new Intent(getApplicationContext(),MultiplicationMedium.class);
+                startActivity(ii);
+
+
+            }
+        });
+
+        Button MultHard = (Button) findViewById(R.id.MultHard);
+        MultHard.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent iii = new Intent(getApplicationContext(),MultiplicationHard.class);
+                startActivity(iii);
+
+
+            }
+        });
+
+
     }
+
+
 }
