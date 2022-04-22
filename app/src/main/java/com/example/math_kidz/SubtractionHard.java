eb837b60e9edc2284f13bf64c22380ed06e670ca
cstewart27 <61986621+cstewart27@users.noreply.github.com>
2022-04-21 20:47:15 -0400

    Added screens

---
+++ app/src/main/java/com/example/math_kidz/SubtractionHard.java
@@ -0,0 +1,14 @@
+package com.example.math_kidz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SubtractionHard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subtraction_hard);
+    }
+}
