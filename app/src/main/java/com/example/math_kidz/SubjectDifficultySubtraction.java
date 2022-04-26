64f684e76b1b438faece700a55c9c0f70a99697a
cstewart27 <61986621+cstewart27@users.noreply.github.com>
2022-04-26 19:37:10 -0400

    updated UI

--- app/src/main/java/com/example/math_kidz/SubjectDifficultySubtraction.java
+++ app/src/main/java/com/example/math_kidz/SubjectDifficultySubtraction.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class SubjectDifficultySubtraction extends AppCompatActivity {
 
@@ -14,6 +15,17 @@ public class SubjectDifficultySubtraction extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subject_difficulty_subtraction);
 
+
+
+        ImageButton subBackArrow = (ImageButton) findViewById(R.id.subBackArrow);
+        subBackArrow.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),Menu.class);
+                startActivity(i);
+
+
+            }
+        });
         Button SubEasy = (Button) findViewById(R.id.SubEasy);
         SubEasy.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
