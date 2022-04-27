c01e258d312114931c0d6fdffff0624ff9153ee1
zriyaz <zakriariyaz@gmail.com>
2022-04-26 23:14:00 -0400

    Zakria - Edit : All topics and difficulty levels have randomly generated questions

--- app/src/main/java/com/example/math_kidz/AdditionEasy.java
+++ app/src/main/java/com/example/math_kidz/AdditionEasy.java
@@ -3,6 +3,10 @@ package com.example.math_kidz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -19,9 +23,31 @@ public class AdditionEasy extends AppCompatActivity {
         //generate random values from 0-9
         int AddRand1 = rand.nextInt(upperbound);
         int AddRand2 = rand.nextInt(upperbound);
-
+        int Answer= AddRand1+AddRand2;
+        String CheckAnswer= String.valueOf(Answer);
         TextView textView = (TextView) findViewById(R.id.AdditionTextView);
         textView.setText(AddRand1 + " + " + AddRand2);
+
+
+        EditText UserInput   = (EditText)findViewById(R.id.AddEasyInput);
+//if (UserInput.getText().toString().equals(Answer))
+
+        Button AnswerChecker = (Button) findViewById(R.id.CheckButton);
+        AnswerChecker.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+               // EditText UserInput = (EditText) findViewById(R.id.AddEasyInput);
+
+                if (UserInput.getText().toString().equals(CheckAnswer)){
+                    textView.setText("NICE!");
+                }
+            }
+        });
+
+
+
+        //if (mEdit.getText().toString().equals(Answer)) {
+         //   textView.setText("NICE!");
+        }
     }
 
-}
+//}
